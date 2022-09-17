cc269901f05ddf76bd0af0b9ba68a5828f19e0ea
Admin <Admin@192.168.0.103>
2022-09-17 18:03:42 +0530

    Commit on 17/09

--- src/main/java/springmvc/controller/HomeController.java
+++ src/main/java/springmvc/controller/HomeController.java
@@ -3,7 +3,9 @@ package springmvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class HomeController {
@@ -30,4 +32,24 @@ public class HomeController {
 		 andView.setViewName("help");
  return andView;		
 	}
+	
+	@RequestMapping("/search")
+  public String search()
+	{
+		
+		
+	 return "search";	
+	}
+	
+	@RequestMapping("/mysearch")
+	  public RedirectView mysearch(@RequestParam("querybox") String querybox)
+		{
+		
+		   String url="https://www.google.com/search?q="+querybox;
+			RedirectView redirectView = new RedirectView();
+			redirectView.setUrl(url);
+		 return redirectView;	
+		}
+		
+	
 }
